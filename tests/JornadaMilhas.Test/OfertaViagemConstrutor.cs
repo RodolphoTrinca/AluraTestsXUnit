9eff2bf997501cd4e6932368f9a8de02c6b8f329
Rodolpho Trinca <rodolphotrinca@hotmail.com>
2024-03-12 12:14:12 -0300

    Testes iniciais

---
+++ tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
@@ -0,0 +1,46 @@
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemConstrutor
+    {
+        [Fact]
+        public void RetornaOfertaValidaQuandoDadosValidos()
+        {
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100.0;
+
+            var validacao = true;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Equal(validacao, oferta.EhValido);
+        }
+
+        [Fact]
+        public void RetornaMensagemDeErroDeRotaOuPeriodoInvalidosQuandoRotaNula()
+        {
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100.0;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
+        }
+
+        [Fact]
+        public void RetornaMensagemDeErroDePrecoInvalidoQuandoPrecoMenorQueZero()
+        {
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = -100.0;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+        }
+    }
+}
